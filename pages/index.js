9242351f20a879ba9d7be27a16dcf525555c2cfd
EdoardoCisco <ciscoedoardo@gmail.com>
2022-06-29 22:42:26 +0200

    second commit

--- pages/index.js
+++ pages/index.js
@@ -58,33 +58,6 @@ const locandineSlide = [
 
 ];
 
-const descrizioneGruppo = [
-  <p className="text-justify text-white px-6 pw-2 max-w-2xl font-sans justify-center " style={{LogoGroup}}>
-La band fonda le basi nel 2014, trovando radici nella provincia vicentina.
-Fin dagli albori, spinti dal desiderio di creare qualcosa di autentico iniziano a scrivere musica propria.
-Nel 2015 il trio formato da Sebastiano Fracasso (vocals & guitar), Giulio Zonin (bass), Elia Rolenti (drums)
- si completa con l’inserimento del chitarrista lead e secondo cantante Emanuele Tonin.
-La band al completo inizia fin da subito a calcare i palchi della zona e non solo.<br/>
-
-Nel settembre 2015 (all’età di 16/17 anni) dopo la prima esperienza in studio pubblicano un Ep contenente i primi 4 pezzi della band,
-che esce di produzione dopo pochi mesi.
-Nel 2016 partecipano al Vicenza Rock Contest vincendo il premio “miglior band Metal”.
-Nel 2017 pubblicano il primo singolo e videoclip “My disgrace”.
-Nel 2018, grazie ad un finanziamento musicraiser della fanbase, registrano e pubblicano il loro primo full length album “We Are What We Are”,
-contenente dieci pezzi, registrato in Produzioni fantasma (VI), 
-presentato al Bocciodromo di Vicenza. Il sound fresco e moderno di We Are What We Are viene apprezzato sopratutto dal pubblico e dagli ascoltatori.<br/>
-
-Grazie al primo disco la band si fa conoscere in tutto il centro nord Italia, continuando ininterrottamente per due anni l’attività live.
-Fra i primi posti negli ascolti internazionali, dopo l’Italia, si trovano Spagna, Ungheria, Canada e Usa.
-Dal 2019 entrano nel bill ufficiale Orion Agency, guidati da Matteo Baroni.<br/>
-
-Nel 2020 esce il singolo e videoclip “Borderline”, linea di confine che segna il passaggio della band ad un nuovo capitolo della sua esistenza.
-Dal 2021 ad oggi iniziano la stesura del nuovo disco, con termine dei lavori al 2022. 
-Questo nuovo lavoro discografico li impegna nella creazione di uno stile compositivo e sonoro completamente autentico.
-Gli Element dal 2015 hanno collezzionato decine di live in locali ed open air, condividendo il palco con nomi come Tom Angelripper, Onkel Tom (Sodom), Fleshgod Apocalypse, Methedras, Ulvedharr, Kanseil, Sinphobia, A Tear Beyond e altri.
-  </p>
-];
-
 
 export default function elementPage(){
   return (
