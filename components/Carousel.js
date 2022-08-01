5e4e7d70c2bfb2b3e8e4ac34606bef3fe274be75
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-08-01 10:39:23 +0200

    carousel fix

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,27 +1,29 @@
 import Image from "next/image";
 import {BsCaretLeftFill,BsCaretRightFill} from "react-icons/bs"   
 
-const Carousel = ({list,state,setState,height,width}) =>{
+const Carousel = ({list,state,setState,height,width,llen}) =>{
 
   // Callback per tornare indietro di uno in array circolare 
-  const goBack = (setState,state,list) =>{
-    if(state==0) setState(list.lenght)
-    else setState(state-1)
+  const goBack = async (setState,state,list) =>{
+    if((state -1)== -1) await setState(llen-1)
+    else await setState(state-1)
+    // console.log(state)
   };
 
   // Callback per tornare indietro di uno in array circolare 
-  const goNext = (setState,state,list) =>{
-    if(state==list.lenght) setState(0)
-    else setState(state+1)
+  const goNext = async (setState,state,list) =>{
+    if((state + 1) ==(llen)) await  setState(0)
+    else await setState(state+1)
+    // console.log(state)
   };
 
   return ( // BackButton -- Immagine -- NextButton 
     <div className="flex flex-row justify-center items-stretch h-1/3 w-full  ">
-      <div className="hover:text-white text-black flex items-center justify-center" onClick={() => goBack(setState,state,list)}>
+      <div className="hover:text-white text-black flex items-center justify-center" onClick={ async () => await goBack(setState,state,list)}>
         <BsCaretLeftFill size={40}/>
       </div>
       <Image src={list[state]} width={width} height={height}/>
-      <div className="hover:text-white text-black flex items-center justify-center"  onClick={() => goNext(setState,state,list)}>
+      <div className="hover:text-white text-black flex items-center justify-center"  onClick={ async () => await goNext(setState,state,list)}>
         <BsCaretRightFill size={40}/>
       </div>
      </div>
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import YouTube from "react-youtube";
 import Carousel from "../components/Carousel.js";
 
 const LogoGroup = {
-  backgroundImage:"url('/logo/logo4.svg')",
-  height:"100%",
-  width:"100%",
+  backgroundImage: "url('/logo/logo4.svg')",
+  height: "100%",
+  width: "100%",
 
 };
 //layout:"fill",(()=>
@@ -61,78 +61,86 @@ const locPath = [
 
 ];
 
+// const getLenght = (array) => {
+//   let llen
+//   while(array[llen] != "") llen++
+//   return llen
+// };
+
 //export default class YoutubeVideo 
 
-export default function elementPage(){
+export default function elementPage() {
   const [btnImg, setBtnImg] = useState(0) //var per img  
   const [btnLoc, setBtnLoc] = useState(0) //var per img  
-  
-//  render() {
-  
-    const opts = {
-      height: "640",
-      width: "1290",
-      playerVars: {
-        autoplay: 1,
-      },
-_onReady(event) {
-  event.target.pauseVideo();
-}
+
+  //  render() {
+
+  const opts = {
+    height: "640",
+    width: "1290",
+    playerVars: {
+      autoplay: 1,
+    },
+    _onReady(event) {
+      event.target.pauseVideo();
     }
- 
+  }
+
   return (
-  <div className="w-screen bg-bgElement">
-    <div className="flex flex-col items-center space-y-16 pt-10">
-      <div className="flex flex-row items-center gap-x-48 ">
-        <Image className=" shadow-none" src="/logo/logo.jpg" width={220} height={200}/>
-        <Image className="" src="/logo/logo.jpg" width={220} height={200}/>
+    <div className="w-screen bg-bgElement">
+      <div className="flex flex-col items-center space-y-16 pt-10">
+        <div className="flex flex-row items-center gap-x-48 ">
+          <Image className=" shadow-none" src="/logo/logo.jpg" width={220} height={200} />
+          <Image className="" src="/logo/logo.jpg" width={220} height={200} />
+        </div>
+        <div>
+          <p className="text-justify text-white px-6 pw-2 max-w-7xl font-sans"  >
+            La band fonda le basi nel 2014, trovando radici nella provincia vicentina.
+            Fin dagli albori, spinti dal desiderio di creare qualcosa di autentico iniziano a scrivere musica propria.
+            Nel 2015 il trio formato da Sebastiano Fracasso (vocals & guitar), Giulio Zonin (bass), Elia Rolenti (drums)
+            si completa con l’inserimento del chitarrista lead e secondo cantante Emanuele Tonin.
+            La band al completo inizia fin da subito a calcare i palchi della zona e non solo.<br />
+
+            Nel settembre 2015 (all’età di 16/17 anni) dopo la prima esperienza in studio pubblicano un Ep contenente i primi 4 pezzi della band,
+            che esce di produzione dopo pochi mesi.
+            Nel 2016 partecipano al Vicenza Rock Contest vincendo il premio “miglior band Metal”.
+            Nel 2017 pubblicano il primo singolo e videoclip “My disgrace”.
+            Nel 2018, grazie ad un finanziamento musicraiser della fanbase, registrano e pubblicano il loro primo full length album “We Are What We Are”,
+            contenente dieci pezzi, registrato in Produzioni fantasma (VI),
+            presentato al Bocciodromo di Vicenza. Il sound fresco e moderno di We Are What We Are viene apprezzato sopratutto dal pubblico e dagli ascoltatori.<br />
+
+            Grazie al primo disco la band si fa conoscere in tutto il centro nord Italia, continuando ininterrottamente per due anni l’attività live.
+            Fra i primi posti negli ascolti internazionali, dopo l’Italia, si trovano Spagna, Ungheria, Canada e Usa.
+            Dal 2019 entrano nel bill ufficiale Orion Agency, guidati da Matteo Baroni.<br />
+
+            Nel 2020 esce il singolo e videoclip “Borderline”, linea di confine che segna il passaggio della band ad un nuovo capitolo della sua esistenza.
+            Dal 2021 ad oggi iniziano la stesura del nuovo disco, con termine dei lavori al 2022.
+            Questo nuovo lavoro discografico li impegna nella creazione di uno stile compositivo e sonoro completamente autentico.
+            Gli Element dal 2015 hanno collezzionato decine di live in locali ed open air, condividendo il palco con nomi come Tom Angelripper, Onkel Tom (Sodom), Fleshgod Apocalypse, Methedras, Ulvedharr, Kanseil, Sinphobia, A Tear Beyond e altri.
+          </p>
+        </div>
+        <YouTube videoId="sTnm5jvjgjM" opts={opts} />
+
+        <Carousel
+          className="flex justify-center my-10"
+          list={fotoPath}
+          state={btnImg}
+          setState={setBtnImg}
+          width={900}
+          height={500}
+          llen={16}
+        />
+
+        <Carousel
+          className="flex justify-center my-10"
+          list={locPath}
+          state={btnLoc}
+          setState={setBtnLoc}
+          width={400}
+          height={600}
+          llen={10}
+        />
       </div>
-      <div>
-        <p className="text-justify text-white px-6 pw-2 max-w-7xl font-sans"  >
-        La band fonda le basi nel 2014, trovando radici nella provincia vicentina.
-        Fin dagli albori, spinti dal desiderio di creare qualcosa di autentico iniziano a scrivere musica propria.
-        Nel 2015 il trio formato da Sebastiano Fracasso (vocals & guitar), Giulio Zonin (bass), Elia Rolenti (drums)
-        si completa con l’inserimento del chitarrista lead e secondo cantante Emanuele Tonin.
-        La band al completo inizia fin da subito a calcare i palchi della zona e non solo.<br/>
-
-        Nel settembre 2015 (all’età di 16/17 anni) dopo la prima esperienza in studio pubblicano un Ep contenente i primi 4 pezzi della band,
-        che esce di produzione dopo pochi mesi.
-        Nel 2016 partecipano al Vicenza Rock Contest vincendo il premio “miglior band Metal”.
-        Nel 2017 pubblicano il primo singolo e videoclip “My disgrace”.
-        Nel 2018, grazie ad un finanziamento musicraiser della fanbase, registrano e pubblicano il loro primo full length album “We Are What We Are”,
-        contenente dieci pezzi, registrato in Produzioni fantasma (VI), 
-        presentato al Bocciodromo di Vicenza. Il sound fresco e moderno di We Are What We Are viene apprezzato sopratutto dal pubblico e dagli ascoltatori.<br/>
-
-        Grazie al primo disco la band si fa conoscere in tutto il centro nord Italia, continuando ininterrottamente per due anni l’attività live.
-        Fra i primi posti negli ascolti internazionali, dopo l’Italia, si trovano Spagna, Ungheria, Canada e Usa.
-        Dal 2019 entrano nel bill ufficiale Orion Agency, guidati da Matteo Baroni.<br/>
-
-        Nel 2020 esce il singolo e videoclip “Borderline”, linea di confine che segna il passaggio della band ad un nuovo capitolo della sua esistenza.
-        Dal 2021 ad oggi iniziano la stesura del nuovo disco, con termine dei lavori al 2022. 
-        Questo nuovo lavoro discografico li impegna nella creazione di uno stile compositivo e sonoro completamente autentico.
-        Gli Element dal 2015 hanno collezzionato decine di live in locali ed open air, condividendo il palco con nomi come Tom Angelripper, Onkel Tom (Sodom), Fleshgod Apocalypse, Methedras, Ulvedharr, Kanseil, Sinphobia, A Tear Beyond e altri.
-        </p>
-     </div>
-     <YouTube videoId="sTnm5jvjgjM"opts={opts}/>
-
-     <Carousel 
-      className="flex justify-center my-10" 
-      list={fotoPath}
-      state={btnImg} 
-      setState={setBtnImg}
-      width={900}
-      height={500}
-      />
-
-     <Carousel
-      className="flex justify-center my-10" 
-      list={locPath}
-      state={btnLoc}
-      setState={setBtnLoc}
-      width={400}
-      height={600}
-      />
-    </div>
-  </div>)
-} 
+    </div>)
+}
 
